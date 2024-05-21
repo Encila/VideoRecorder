fe6f281504498538c57c1aab997e18f757778406
Encila <78430229+Encila@users.noreply.github.com>
2024-05-21 17:07:08 +0200

    Refacto

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import time
 import os
 from datetime import datetime
-from picamera2 import Picamera2, Preview
-from picamera2.encoders import H264Encoder
-import subprocess
+from picamera2 import Picamera2
 
 def enregistrer_video(duree):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -12,33 +10,16 @@ def enregistrer_video(duree):
     base_path = os.path.join(home_dir, "Desktop", "database")
     os.makedirs(base_path, exist_ok=True)
     
-    fichier_h264 = os.path.join(base_path, f"video_{timestamp}.h264")
-    fichier_mp4 = os.path.join(base_path, f"video_{timestamp}.mp4")
+    fichier = os.path.join(base_path, f"video_{timestamp}.mp4")
 
     picam2 = Picamera2()
     video_config = picam2.create_video_configuration(main={"size": (2592, 2592)})
-    picam2.configure(video_config)
-
-    encoder = H264Encoder(10000000)
-
-    picam2.start_preview(Preview.QTGL)
-    picam2.start_recording(encoder, fichier_h264)
-    
+        
     print(f"Enregistrement de la vidéo pendant {duree} secondes...")
-    time.sleep(duree)
-    picam2.stop_recording()
-    picam2.stop_preview()
-    picam2.close()
 
-    print(f"Conversion de {fichier_h264} en {fichier_mp4}...")
+    picam2.start_and_record_video(fichier, duration=duree, config=video_config, show_preview=True)
 
-    # Conversion h264 -> mp4
-    commande_ffmpeg = f"ffmpeg -r 30 -i {fichier_h264} -c:v copy {fichier_mp4}"
-    subprocess.run(commande_ffmpeg, shell=True)
-
-    os.remove(fichier_h264)
-
-    print(f"Enregistrement terminé. Vidéo sauvegardée sous {fichier_mp4}")
+    print(f"Enregistrement terminé. Vidéo sauvegardée sous {fichier}")
 
 if __name__ == "__main__":
     duree_video = 15  # durée en secondes
