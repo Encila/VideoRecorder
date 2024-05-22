b0b209662d38bad00146f0ca5ba6e8a2b2c33bb3
Encila <78430229+Encila@users.noreply.github.com>
2024-05-22 14:39:15 +0200

    reprise du fichier Picamera officiel (voir github)

--- test.py
+++ test.py
@@ -1,15 +1,59 @@
-from PyQt5.QtWidgets import QApplication
-from picamera2.previews.qt import QGLPicamera2
+#!/usr/bin/python3
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QPushButton,
+                             QVBoxLayout, QWidget)
+
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder, FfmpegEncoder
+from picamera2.outputs import FileOutput
+from picamera2.previews.qt import QGlPicamera2
+
+
+def post_callback(request):
+    label.setText(''.join(f"{k}: {v}\n" for k, v in request.get_metadata().items()))
+
 
 picam2 = Picamera2()
-
-picam2.configure(picam2.create_preview_configuration())
+picam2.post_callback = post_callback
+picam2.configure(picam2.create_video_configuration(main={"size": (2592, 2592)}))
 
 app = QApplication([])
-qpicamera2 = QGLPicamera2(picam2, width=800, height=600, keep_ar=False)
-qpicamera2.setWindowTitle("Qt Picamera2 App")
+
+
+def on_button_clicked():
+    global recording
+    if not recording:
+        encoder = FfmpegEncoder("mp4", 10000000)
+        output = FileOutput("test.mp4")
+        picam2.start_encoder(encoder, output)
+        button.setText("Stop recording")
+        recording = True
+    else:
+        picam2.stop_encoder()
+        button.setText("Start recording")
+        recording = False
+
+
+qpicamera2 = QGlPicamera2(picam2, width=800, height=480, keep_ar=False)
+button = QPushButton("Start recording")
+button.clicked.connect(on_button_clicked)
+label = QLabel()
+window = QWidget()
+window.setWindowTitle("Qt Picamera2 App")
+recording = False
+
+label.setFixedWidth(400)
+label.setAlignment(QtCore.Qt.AlignTop)
+layout_h = QHBoxLayout()
+layout_v = QVBoxLayout()
+layout_v.addWidget(label)
+layout_v.addWidget(button)
+layout_h.addWidget(qpicamera2, 80)
+layout_h.addLayout(layout_v, 20)
+window.resize(1200, 480)
+window.setLayout(layout_h)
 
 picam2.start()
-qpicamera2.show()
+window.show()
 app.exec()
