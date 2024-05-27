6fecc48ac90f544f83bc9820585275c7b0ac15c1
Encila <78430229+Encila@users.noreply.github.com>
2024-05-27 10:49:27 +0200

    test alex

--- test.py
+++ test.py
@@ -5,10 +5,14 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 
 picam2 = Picamera2()
+
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
 encoder = H264Encoder(10000000)
+
+encoder.width = 100
+encoder.height = 100
 
 picam2.start_recording(encoder, 'test.h264')
 time.sleep(10)
